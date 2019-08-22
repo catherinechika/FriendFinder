52d38abf36302cc8ff37a42dca80f44fd335b890
Chika <catherine.achukwu@gmail.com>
2019-08-22 15:51:20 -0400

    Home route updated for htmlroutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,7 @@
 module.exports = function (app, path) {
     var answersArray = []
 
-    app.get("/home", function (req, res) {
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
